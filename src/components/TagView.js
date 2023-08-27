1a64f3e367d37b2de075c3d973760f3b9995fd9e
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-08-27 11:20:30 +0530

    create expand and show inpuut for tagview

--- src/components/TagView.js
+++ src/components/TagView.js
@@ -4,6 +4,15 @@ import Action from './Action';
 
 const TagView = ({ tree }) => {
   const [input, setInput] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [showInput, setShowInput] = useState(false);
+  const [expandArrow, setExpandArrow] = useState(false);
+
+  const handleNewChild = () => {
+     setExpandArrow(!expandArrow); 
+
+    setShowInput(true);
+  };
 
   const onAddComment = () => {
     // Implement your logic for adding a comment here
@@ -28,13 +37,44 @@ const TagView = ({ tree }) => {
           <>
             <span style={{ wordWrap: "break-word" }}>{tree.name}</span>
             <div style={{ display: 'flex', marginTop: 5 }}>
-              <Action className="reply" type="Add" />
-              <Action className="reply" type="Edit" />
-              <Action className="reply" type="Delete" /></div>
+              {edit ? (
+                <>
+                  <Action className="reply" type="Save" />
+                  <Action className="reply" type="Cancel" handleClick={() => setEdit(false)} />
+                </>
+              ) : (
+                <>
+                  <Action className="reply"
+                    type={
+                      <>
+                        {expandArrow ? (
+                          <BiSolidDownArrow />
+                        ) : (<BiSolidRightArrow />)}{" "} Add
+                      </>
+                    }
+                    handleClick={handleNewChild} />
+                  <Action className="reply" type="Edit" handleClick={() => setEdit(true)} />
+                  <Action className="reply" type="Delete" />
+                </>
+              )}
+            </div>
           </>
         )}
       </div>
-      <div style={{ paddingLeft: 25 }}>
+      <div style={{display: expandArrow?"block" : 'none', paddingLeft: 25 }}>
+        {showInput && (
+          <div className='inputContainer'>
+            <input
+              type='text'
+              className='inputContainer__input first_input'
+              autoFocus
+              onChange={(e) => setInput(e.target.value)}
+              placeholder='type...'
+            />
+            <Action className="reply" type="Add" />
+            <Action className="reply" type="Cancel" handleClick={() => { setShowInput(false); setExpandArrow(false); }} />
+          </div>
+        )}
         {tree?.items?.map((t) => { return <TagView key={t.id} tree={t} /> })}
       </div>
     </div>
